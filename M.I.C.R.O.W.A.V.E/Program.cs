a56523bf87cbcc6534cedb0f42a3728e7209973a
realdenis <realdenismj@gmail.com>
2023-04-02 22:46:36 +0700

    added microwave(project manager, still kinda broken)

---
+++ M.I.C.R.O.W.A.V.E/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
+
+/*
+ * microwave build to build a project.
+ * microwave start {project_name} to make config file.
+ */ 
+
+namespace M.I.C.R.O.W.A.V.E
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[0] == "build")
+            {
+                if ((File.Exists("lib/Engine.lib")) && Directory.Exists("include"))
+                {
+                    string ProjectName = File.ReadAllLines("ProConf")[0];
+                    Process.Start("dontremove");
+                }
+                else
+                {
+                    Console.WriteLine(".lib or include/ does not exist. ");
+                }
+            }
+            else if (args[0] == "start")
+            {
+                if ((File.Exists("lib/Engine.lib")) && Directory.Exists("include"))
+                {
+                    string ProjectName = args[1];
+                    File.WriteAllText("ProConf", ProjectName);
+                    File.WriteAllText("dontremove", $"g++ main.cpp -I./include -L./lib -lEngine -o {ProjectName}");
+                }
+                else
+                {
+                    Console.WriteLine(".lib or include/ does not exist. ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not a command. ");
+            }
+        }
+    }
+}
